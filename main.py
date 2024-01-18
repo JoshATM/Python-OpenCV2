f9ed48d7d1229c61ad19e8cbefff82834679f125
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-01-18 15:16:25 +0000

    10/01/2024

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import cv2 # pip install opencv-python
 
 capture = cv2.VideoCapture(0)
-detail = 1
+detail = .1 # detail of ascii art (0.1 - 1)
 ScreenX = round(detail*capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 ScreenY = round(detail*capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -15,10 +15,12 @@ def whole_to_ascii(inFrame):
 
 while cv2.waitKey(1):
     ret, frame = capture.read() # get each frame
-    greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # colour frame to grey
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # colour frame to grey
     
-    newFrame = cv2.resize(greyFrame, (int(ScreenX), int(ScreenY))) # resize frame to fit screen
+    newFrame = cv2.resize(grayFrame, (int(ScreenX), int(ScreenY))) # resize frame to fit screen
+    
     finalFrame = whole_to_ascii(newFrame) # convert frame to ascii
     os.system("clear") # clear screen
-    print("\n".join("".join(row) for row in finalFrame)) # print frame
-    cv2.imshow('frame') # show frame
+    print("\n".join((''.join(row) for row in finalFrame)), end='') # print frame
+    
+    cv2.imshow('frame', newFrame) # show frame
