733440e0816ad0d8c52cabb96fc4527d57d08505
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-01-18 14:47:08 +0000

    10/01/2024

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ detail = 1
 ScreenX = round(detail*capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 ScreenY = round(detail*capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+def row_to_ascii(row):
+    order = (' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o', 'k', 'X', 'd', 'O', '0', 'K', 'N')
+    return tuple(order[int(x / (255 / 16))] for x in row)[::-1]
+
+def whole_to_ascii(inFrame):
+    return tuple(row_to_ascii(row) for row in inFrame)
 
 while cv2.waitKey(1):
     ret, frame = capture.read()
